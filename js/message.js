90b38cb78509dcd9af58d5018993335c8d39747d
Helin Akdoğan <akdoganhelin@gmail.com>
2023-02-11 04:14:52 +0300

    11.02.2023

---
+++ js/message.js
@@ -0,0 +1,14 @@
+
+alert("JS FILE IS REACHED");
+
+function call() {
+    alert("alert!!!");
+}
+
+function goodbye() {
+    alert("goodbye :)");
+}
+
+function welcome() {
+    alert("welcome :)");
+}
