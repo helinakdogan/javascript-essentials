b04685adebf899dcc1d946c4e13da8d0789a9186
Helin Akdoğan <akdoganhelin@gmail.com>
2023-02-26 02:53:26 +0300

    26.02.2023

---
+++ js/lib.js
@@ -0,0 +1,5 @@
+export const PI = 3.14;
+
+export const sum = (x,y) => {return x+y;};
+export const square = (x) => {return x*x;};
+export const hypotenuse = (x,y) => {return Math.sqrt(square(x)+square(y));};
---
+++ lesson1_2_1.js
@@ -0,0 +1,3 @@
+import * as lib from './js/lib';
+
+$('#result').innerHTML = lib.sum(6,7);
